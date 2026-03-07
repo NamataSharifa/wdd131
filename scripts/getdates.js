1bbf6fd1325b9c759d1afce6aa2ad33cde229ec7
namatasharifa <namatasharifa0@gmail.com>
2026-03-07 19:04:24 +0200

    added getdates.js

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// current year
+const year = new Date().getFullYear();
+document.getElementById("current-year").textContent = year;
+
+// last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
