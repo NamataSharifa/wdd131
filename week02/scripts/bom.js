8bcb9cb6c8ba6b2f622f92bdf3f5444ebeec33a3
namatasharifa <namatasharifa0@gmail.com>
2026-03-15 20:40:04 +0200

    updated bom.js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -4,22 +4,32 @@ const list = document.querySelector('#list'); // unordered list reference
 
 button.addEventListener('click', function () {
 
-    if (input.value !== '') {
-
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
-
-        li.textContent = input.value;
-
-        deleteButton.textContent = '❌';
-        deleteButton.setAttribute('aria-label', 'Remove ' + input.value);
-
-        li.append(deleteButton);
-
-        list.append(li);
-
-        input.value = '';
+    // Check that input is not blank
+    if (input.value.trim() === '') {
         input.focus();
+        return;
     }
 
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = input.value;
+
+    deleteButton.textContent = '❌';
+    deleteButton.setAttribute('aria-label', 'Remove ' + input.value);
+
+    li.append(deleteButton);
+    list.append(li);
+
+    // Event listener to remove chapter
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        input.focus();
+    });
+
+    // Clear input field
+    input.value = '';
+
+    // Return focus to input
+    input.focus();
 });
